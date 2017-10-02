8be7d0d4719ce7282fc60aa5f9644f68cbcacb28
Carlos Gomez <carleng28@gmail.com>
2017-10-02 18:25:46 -0400

    second commit

--- src/databaseweek4/DatabaseWeek4.java
+++ src/databaseweek4/DatabaseWeek4.java
@@ -21,6 +21,7 @@ public class DatabaseWeek4 {
      * @param args the command line arguments
      * @throws java.sql.SQLException
      */
+    //TEST
     public static void main(String[] args) throws SQLException {
         // TODO code application logic here
 
